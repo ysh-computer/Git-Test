0f9ca6602d97c8e69288bed0f58f812e2fbf98ee
para7 <minstrel.suppin@gmail.com>
2015-11-24 19:30:15 +0900

    Revert "Masterlocal"

--- Hello.cpp
+++ Hello.cpp
@@ -1,40 +1,27 @@
 #include <iostream>
 #include <string>
 
-namespace Hello
-{
-	class hello
-    {
+namespace Hello{
+	class hello{
 	private:
 		std::string str;
 	public:
 		hello() : str("Hello"){}
 
-		hello(std::string str)
-        {
-            this->str = str;
-        }
+		hello(std::string str){this->str = str;}
 
-		void Print()
-        {
+		void Print(){
 			std::cout << str << "\n";
 		}
-        void Add(std::string str2)
-        {
-            str.append(str2);
-        }
 	};
 }
 
-int main()
-{
+int main(){
     std::ios::sync_with_stdio();
     std::cin.tie(0);
 	Hello::hello hellodefault;
 	hellodefault.Print();
 	Hello::hello test("ファンタジスタドール");
 	test.Print();
-    test.Add("に文字列を付け加える");
-    test.Print();
 	return 0;
 }
